65e1797f962d5085add92dfb45213cbca256e9d9
G. Carter Stokum <GCStokum@gmail.com>
2016-08-10 04:57:53 -0700

    refactor
    combined parts into one git until more work is done

---
+++ FTAGN.browser/README.md
@@ -0,0 +1,2 @@
+# FTAGN.browser
+FTAGN. or File Tag'n.  A tag cloud based file browser, and tag based internet browser extension.
---
+++ FTAGN.extension/code/fTAGn.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>A small example page to insert some data in to the MySQL database using PHP</h1>
+<form action="insert.php" method="post">
+URL : <input type="text" name="URL" /><br>
+Tag 1 : <input type="text" name="T1" /><br>
+Tag 2 : <input type="text" name="T2" /><br>
+<input type="submit" />
+</form>
+</body>
+</html>
